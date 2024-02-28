ccea949e311ebc1702737090118f7c22d032df6f
Георгий Коряковский <wallacce135@gmail.com>
2024-02-29 01:07:41 +0300

    Initialization

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState<string>("")
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ХУЙ</Text>
       <StatusBar style="auto" />
+      <Button title='Нажми' onPress={() => {}}></Button>
     </View>
   );
 }
